8a910772883212480c7eb237f9e64bc99804644b
LindaTancini <lindatancini95@gmail.com>
2025-02-28 17:13:56 +0100

    feat: add pc random con funzione

---
+++ js-dispari/main.js
@@ -0,0 +1,8 @@
+// utente sceglie un numero da 1 a 5
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log(userNumber);
+
+// pc sceglie numero random con funzione
+function getRandomNumber() {
+  return Math.floor(Math.random() * 5) + 1;
+}
