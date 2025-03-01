65d219da18919af68f052b6029feccbc1fab2ef3
LindaTancini <lindatancini95@gmail.com>
2025-03-01 10:43:25 +0100

    feat: add function palindroma

--- js-pali/main.js
+++ js-pali/main.js
@@ -2,3 +2,15 @@
 
 let word = prompt("Inserisci una parola");
 console.log(word);
+
+// funzione per stabile se parola è palindroma
+function palindroma(word) {
+  for (let i = 0; i < word.length / 2; i++) {
+    // Se i caratteri non corrispondono, non è un palindromo
+    if (word[i] !== word[word.length - i - 1]) {
+      return false;
+    }
+    // Se il ciclo termina senza trovare differenze, è un palindromo
+  }
+  return true;
+}
