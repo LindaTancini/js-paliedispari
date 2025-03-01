9f1877c31f3d2ea7ffef64f149892a4fbbe3e67c
LindaTancini <lindatancini95@gmail.com>
2025-03-01 11:32:14 +0100

    feat: correct with new for

--- js-pali/main.js
+++ js-pali/main.js
@@ -5,14 +5,15 @@ console.log(word);
 
 // funzione per stabile se parola è palindroma
 function palindroma(word) {
-  for (let i = 0; i < word.length / 2; i++) {
-    // Se i caratteri non corrispondono, non è un palindromo
-    if (word[i] !== word[word.length - i - 1]) {
-      return false;
-    }
-    // Se il ciclo termina senza trovare differenze, è un palindromo
+  // creo una stringa che contiene la parola al contrario
+  let reversePalindroma = "";
+  /* Il ciclo inizierà a (word.length - 1) che corrisponde 
+       all'ultimo carattare della stringa. Finché i è maggiore o uguale a 0, il ciclo continua
+       Decrementiamo i dopo ogni iterazione */
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversePalindroma = reversePalindroma += word[i];
   }
-  return true;
+  return reversePalindroma === word;
 }
 
 // è palindroma?
