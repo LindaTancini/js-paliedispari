81dbc00170a514769454698c308483a7ca4604fc
LindaTancini <lindatancini95@gmail.com>
2025-03-03 10:53:56 +0100

    feat: correct in class

--- js-dispari/main.js
+++ js-dispari/main.js
@@ -1,14 +1,23 @@
 // L'utente sceglie pari o dispari
 let userChoice = prompt("pari o dispari?");
-
+console.log(userChoice);
 // utente sceglie un numero da 1 a 5
 let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
 console.log(userNumber);
 
-// pc sceglie numero random con funzione
-function randomPcNumber() {
-  return Math.floor(Math.random() * 5) + 1;
+// creo la funzione per il numero random dato dal pc con max e min
+function randomPcNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // Math.floor(Math.random() * 5) +1
 }
+
+// pc sceglie numero random con funzione
+let computerNumber = randomPcNumber(1, 5);
+console.log(computerNumber);
+
+// somma numeri
+let sum = userNumber + computerNumber;
+console.log(sum);
 
 // controllo se utente inserisce un numero e  pari o dispari
 if (userNumber < 1 || userNumber > 5) {
@@ -16,12 +25,6 @@ if (userNumber < 1 || userNumber > 5) {
 } else if (userChoice !== "pari" && userChoice !== "dispari") {
   console.log("Errore: devi scegliere 'pari' o 'dispari'.");
 }
-
-// somma numeri
-let computerNumber = randomPcNumber();
-let sum = userNumber + computerNumber;
-console.log(computerNumber);
-console.log(sum);
 
 // la somma è pari?
 function isEven(number) {
