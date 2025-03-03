30e903f9c20ae079e010df44aa5149a71227f36a
LindaTancini <lindatancini95@gmail.com>
2025-03-03 11:49:12 +0100

    feat: correct in class

--- js-pali/main.js
+++ js-pali/main.js
@@ -1,24 +1,25 @@
 // chiedo la parola da inserire
-
-let word = prompt("Inserisci una parola");
-console.log(word);
-
-// funzione per stabile se parola è palindroma
-function palindroma(word) {
-  // creo una stringa che contiene la parola al contrario
-  let reversePalindroma = "";
-  /* Il ciclo inizierà a (word.length - 1) che corrisponde 
-       all'ultimo carattare della stringa. Finché i è maggiore o uguale a 0, il ciclo continua
-       Decrementiamo i dopo ogni iterazione */
-  for (let i = word.length - 1; i >= 0; i--) {
-    reversePalindroma = reversePalindroma += word[i];
-  }
-  return reversePalindroma === word;
-}
+let userWord = prompt("Inserisci una parola");
+console.log(userWord);
+const isWordPalindroma = palindroma(userWord);
 
 // è palindroma?
-if (palindroma(word)) {
+if (isWordPalindroma) {
   alert("La parola è palindroma");
 } else {
   alert("La parola non è palindroma");
+}
+
+// funzione per stabile se parola è palindroma
+function palindroma(input) {
+  const word = input.toLowerCase();
+  // creo una stringa che contiene la parola al contrario
+  let reverse = "";
+  // Il ciclo inizierà a (word.length - 1) che corrisponde all'ultimo carattare della stringa.
+  //Finché i è maggiore o uguale a 0, il ciclo continua
+  //Decrementiamo i dopo ogni iterazione
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverse = reverse += word[i];
+  }
+  return word === reverse;
 }
